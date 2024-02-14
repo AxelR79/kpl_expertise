85139e079c6e6a7c0074f1d26f2dbdd255319839
Axel Renaud <axelr79350@gmail.com>
2024-02-14 12:54:34 +0100

    Test Responsive Navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,20 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    fontFamily: {
+      'navbar': ['"Roboto"', '"Montserrat"', 'Tahoma', 'Verdana', 'sans-serif'],
+    },
+    backgroundColor: theme => ({
+      'red': '#E66352'
+     }),
+    textColor: {
+      'white-50': 'rgba(255, 255, 255, 0.5)',
+      'white': '#fff',
+    },
+    screens: {
+      'bs': '1330px',
+      'ms': '1200px',
+    },
   },
   plugins: [],
 };
